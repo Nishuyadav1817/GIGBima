af4337c08e967b383a811ad893751408697d0d37
nishuya <prathamyadav1817@gmail.com>
2026-03-20 02:25:47 +0530

    enter

--- server/index.js
+++ server/index.js
@@ -1,47 +1,29 @@
- const express=require("express");
-const App=express();
+const express = require("express");
+const App = express();
 App.use(express.json());
-const DataBase=require("./Main/DB");
-const WorkerAuth=require("./Register/Reg")
-const cookiparcer=require('cookie-parser')
-App.use(cookiparcer());
+
+const DataBase = require("./Main/DB");
+const WorkerAuth = require("./Register/Reg");
+
+const cookieParser = require("cookie-parser");
+App.use(cookieParser());
+
 require("dotenv").config();
 const cors = require("cors");
 
-App.use("/worker",WorkerAuth);
 const allowedOrigins = [
   "https://gig-bima.vercel.app/"
-  
 ];
 
 App.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: allowedOrigins,
+  credentials: true
 }));
 
+App.use("/worker", WorkerAuth);
 
+DataBase().then(() => {
+  console.log("DB connected");
+});
 
-
-const BuildConnection=async () => {
-      try{
-          await Promise.all([DataBase()])
-            console.log("DB conneacted")
-
-           App.listen(process.env.PORT,()=>{
-           console.log("App Listen on given port number 16016")
-           })
-
-      }catch(err){
-        console.log(err);
-      }
-}
-
-BuildConnection();
+module.exports = App;
